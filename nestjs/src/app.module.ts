0bb89753e6de9ea16bbf55a24cf5a989e966e450
Bertoldo Klinger <bertoldokbrj@gmail.com>
2023-10-21 22:49:15 +0000

    feat: add docker & dockerfile in project

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BankAccountsModule } from './bank-accounts/bank-accounts.module';
+import { BankAccount } from './bank-accounts/entities/bank-account.entity';
+import { PixKey } from './pix-keys/entities/pix-key.entity';
+import { PixKeysModule } from './pix-keys/pix-keys.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      database: 'nest',
+      username: 'postgres',
+      password: 'root',
+      entities: [BankAccount, PixKey],
+      synchronize: true,
+    }),
+    BankAccountsModule,
+    PixKeysModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PixKeyAlreadyExistsErrorFilter } from './pix-keys/filters/pix-key-already-exists.error';
+import { PixKeyGrpcUnknownErrorFilter } from './pix-keys/filters/pix-key-grpc-unknown-error.error';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(
+    new PixKeyAlreadyExistsErrorFilter(),
+    new PixKeyGrpcUnknownErrorFilter(),
+  );
   await app.listen(3000);
 }
 bootstrap();
